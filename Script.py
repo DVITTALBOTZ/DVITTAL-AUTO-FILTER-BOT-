fabc3b283e6200f1cfa83a5dcb7173e9151d90ed
DVITTALBOTZ <133628140+DVITTALBOTZ@users.noreply.github.com>
2026-01-23 16:59:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -244,7 +244,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @DVITTALBOTZ {file_name}
 
 <b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
 
@@ -754,6 +754,7 @@ Example:- /set_caption 📕 File Name: {filename}
 
 
     
+
 
 
 
